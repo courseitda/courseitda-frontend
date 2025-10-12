7fd0be8d157652fb3dfd701c97a98ffb8c01e258
RaZel713 <razel7130@gmail.com>
2025-10-12 18:55:49 +0900

    refactor: 반영 사항 세부 주석 추가

--- src/features/layout/navigation-drawer.tsx
+++ src/features/layout/navigation-drawer.tsx
@@ -88,12 +88,14 @@ export const NavigationDrawer = ({
           <Menu className="w-5 h-5" />
         </Button>
       </SheetTrigger>
+      {/* UserRequest: 모바일 뷰에서는 전체 화면 가로길이의 3/4 (75%), 데스크톱에서는 고정 너비 320px */}
       <SheetContent side="right" className="w-[75vw] md:w-80 p-0 flex flex-col">
         {/* UserRequest: 네비게이션 드로우바 제목을 가운데 정렬 (!text-center) */}
         <SheetHeader className="p-4 pb-3 !text-center">
           <SheetTitle>메뉴</SheetTitle>
         </SheetHeader>
 
+        {/* UserRequest: 스크롤 영역을 유연하게 확장하고 내부 여백 설정 (좌우 16px, 아래 16px, 자식 요소 간 세로 간격 16px) */}
         <ScrollArea className="flex-1">
           <div className="px-4 pb-4 space-y-4">
             {/* 계정 정보 영역 */}
